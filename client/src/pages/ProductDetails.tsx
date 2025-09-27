7d3d0b66c80f31ed4138c472e7feaa78ebd76232
weebtreasures <weebtreasures@gmail.com>
2025-09-27 13:40:57 +0530

    Change product image to use object-contain
    Updated the main product image styling from 'object-cover' to 'object-contain' to ensure the entire image is visible within its container, improving display for images with varying aspect ratios.

--- client/src/pages/ProductDetails.tsx
+++ client/src/pages/ProductDetails.tsx
@@ -104,7 +104,7 @@ export default function ProductDetails() {
               <img 
                 src={product.images[currentImageIndex]} 
                 alt={product.name} 
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" 
+                className="w-full h-full object-contain transition-transform duration-300 group-hover:scale-105" 
               />
             </div>
             
